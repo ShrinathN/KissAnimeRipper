45cf6f0f96cb4b4f653f89872fc96a302b4d9b5d
Shrinath Nimare <shrinath.nimare@gmail.com>
2019-05-05 17:13:53 +0530

    Fixed bug where no links were returned if 480p or 720p links could not be found

--- scripts/rapidvideo_download_link.js
+++ scripts/rapidvideo_download_link.js
@@ -16,15 +16,29 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   }
 });
 
+var downloadLink; //setting download link as empty array
+
+function saveLinkAndContinue() {
+  //sends the download links list
+  chrome.runtime.sendMessage({
+    todo: "addDownloadLink",
+    data: downloadLink
+  });
+  //asks for the next page to go to
+  chrome.runtime.sendMessage({
+    todo: "whatIsNextPage"
+  });
+}
+
 //this function runs for the first time when the yesCanRun message is received
 function ripLink() {
+  downloadLink = [];
+
   var downloadButtons = document.getElementsByClassName("button_small tooltip"); //this is the array of buttons with the download links
   if (downloadButtons == undefined) { //probably means page hasn't finished loading
     window.setTimeout(ripLink, 1000); //execute itself again 1 second later
     return; //exit
   }
-
-  var downloadLink = []; //setting download link as empty array
 
   if(quality == "360") { //lowest quality 360p or lowest available quality
     downloadLink.push(downloadButtons[0].href);
@@ -35,6 +49,11 @@ function ripLink() {
         break;
       }
     }
+    if(downloadLink[0] == undefined) {
+      downloadLink.push(downloadButtons[0].href);
+    }
+    saveLinkAndContinue();
+    return;
   } else if(quality == "720") { //720p, starting from the last, to the beginning
     for(var i = downloadButtons.length-2; i > -1; i--) {
       if(downloadButtons[i].href.includes("720")) {
@@ -42,10 +61,15 @@ function ripLink() {
         break;
       }
     }
+    if(downloadLink[0] == undefined) {
+      downloadLink.push(downloadButtons[0].href);
+    }
+    saveLinkAndContinue();
+    return;
   } else if(quality == "1080") { //highest quality 1080p, or best available quality
     downloadLink.push(downloadButtons[downloadButtons.length - 2].href);
   }
-
+}
   /*
   var downloadLink = []; //setting download link as empty array
   for (i = 0; i < downloadButtons.length - 1; i++) {
@@ -58,17 +82,6 @@ function ripLink() {
     downloadLink.push(downloadButtons[0].href);
   }
   */
-
-  //sends the download links list
-  chrome.runtime.sendMessage({
-    todo: "addDownloadLink",
-    data: downloadLink
-  });
-  //asks for the next page to go to
-  chrome.runtime.sendMessage({
-    todo: "whatIsNextPage"
-  });
-}
 
 //asks if it can run, starting the operation of the script
 chrome.runtime.sendMessage({
